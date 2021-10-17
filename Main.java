01fc084bc791f60a8a6aa9d8c188962a2550d626
tremor6916 <lpawaon@gmail.com>
2021-10-17 09:39:39 +0800

    guide and todo placed

--- Main.java
+++ Main.java
@@ -1,7 +1,30 @@
 import java.util.*;
+import java.io.*;
 public class Main {
-  public static void main(String[] args){
+  public static void main(String[] args)throws FileNotFoundException{
       Scanner sc = new Scanner(System.in);
       
+
   }  
 }
+
+/*Contents sa Security Trainee Information System
+- 3 Courses to Apply (PLTC, RTC, BSSC) 
+    - PLTC (Pre-Licensing Training Course) for aspiring guards 
+    - RTC (Refresher Training Course) for renewing guards
+    - BSSC (Basic Security Supervisory Course) for security officers (Lahi ang Security Guard ug Security Officer)
+- Name
+- Age
+- Gender
+- Highest Educational Attainment
+    - Year of Graduation
+- Contact Number
+- Email Address
+
+IF COURSE CHOICE = RTC OR BSSC:
+    - SSS Number (Optional) - pwede ni walay unod specially sa mga mag PLTC pa kay wala pa jud silay TIN kay trainee pa
+    - TIN Number (Optional) - pwede ni walay unod specially sa mga mag PLTC pa kay wala pa jud silay TIN kay trainee pa
+    - SG License Number (For RTC Trainees Only)
+        - Expiry Date of SG License Number
+    - SBR Number (For RTC Trainees Only)
+*/
