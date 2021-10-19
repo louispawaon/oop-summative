3c4ad0b04cc35a765ed35be02f98b7e32e22e8ff
tremor6916 <lpawaon@gmail.com>
2021-10-19 13:21:40 +0800

    initialize pltc class

---
+++ pltc.java
@@ -0,0 +1,34 @@
+public class pltc {
+    private String courseName;
+    private String teacher;
+    private String[] curriculum;
+    //wala pa schedule
+    private double tuition;
+    private float units;
+    private String[] classmates;
+
+    private String getCourse(){
+        return courseName;
+    }
+
+    private String getTeacher(){
+        return teacher;
+    }
+
+    private String[] getCurriculum(){
+        return curriculum;
+    }
+
+    private double getTuition(){
+        return tuition;
+    }
+
+    private float getUnits(){
+        return units;
+    }
+
+    private String[] getClassmates(){
+        return classmates;
+    }
+
+}
