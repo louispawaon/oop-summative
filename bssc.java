baa1139399200c59caec88f7dddb7e4dc8bd5b92
tremor6916 <lpawaon@gmail.com>
2021-10-19 19:36:27 +0800

    update course classes

--- bssc.java
+++ bssc.java
@@ -1,8 +1,9 @@
+import java.util.*;
 public class bssc {
     private static String courseName = "BSSC"; //fixed courseName
     private static String teacher = "Mr. Rommel Pawaon"; //fixed teacher - kay isa ra ka instructor sa amo kada course
     private static String[] curriculum; //same rag curriculum sa isa ka course
-    //wala pa schedule
+    private LinkedHashMap<String, Integer> schedule = new LinkedHashMap<String, Integer>(); //not final dle ko sure 
     private static double tuition = 6000.00; //fixed na daan ang tuition
     private static float units = 3; //fixed units 
     private String[] classmates;
--- pltc.java
+++ pltc.java
@@ -1,8 +1,9 @@
+import java.util.*;
 public class pltc {
     private static String courseName = "PLTC"; //fixed courseName
     private static String teacher = "Mr. Dionisio Carampatana"; //fixed teacher - kay isa ra ka instructor sa amo kada course
     private static String[] curriculum; //same rag curriculum sa isa ka course
-    //wala pa schedule
+    private LinkedHashMap<String, Integer> schedule = new LinkedHashMap<String, Integer>(); //not final dle ko sure 
     private static double tuition = 6000.00; //fixed na daan ang tuition
     private static float units = 4; //fixed units 
     private String[] classmates;
