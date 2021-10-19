e2a0e86c9e3f4b7d787b36b88f9875e87f309d7c
tremor6916 <lpawaon@gmail.com>
2021-10-19 19:22:14 +0800

    update pltc class

--- pltc.java
+++ pltc.java
@@ -1,10 +1,10 @@
 public class pltc {
-    private static String courseName; //fixed courseName
-    private static String teacher; //fixed teacher - kay isa ra ka instructor sa amo kada course
+    private static String courseName = "PLTC"; //fixed courseName
+    private static String teacher = "Mr. Dionisio Carampatana"; //fixed teacher - kay isa ra ka instructor sa amo kada course
     private static String[] curriculum; //same rag curriculum sa isa ka course
     //wala pa schedule
-    private static double tuition; //fixed na daan ang tuition
-    private static float units; //fixed units 
+    private static double tuition = 6000.00; //fixed na daan ang tuition
+    private static float units = 4; //fixed units 
     private String[] classmates;
 
     private String getCourse(){
